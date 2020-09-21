74518d0b44230b046f21852b7376bbf161494bfc
Chitrangda Kapoor <chitrangda.kapoor@vibrato.com.au>
2020-09-21 14:26:42 +1000

    updated

--- .circleci/scripts/auto_merge.py
+++ .circleci/scripts/auto_merge.py
@@ -2,26 +2,25 @@
 
 import os 
 import subprocess
+import re
 
+# branches = subprocess.Popen('git branch --list \'release/*\' ' , shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+# print(branches.stdout.readlines())
+# squared = list(map(lambda x: x**2, branches))
 branches_flow = ['release/3.1','release/3.2', 'release/3.3']
 
 def automatic_merge(current_branch, branches_flow):
     print('Auto-merging through: %s' % branches_flow)
     branch_index = branches_flow.index(current_branch)
-    print (branch_index)
-    print(len(branches_flow)-1)
     if branch_index < (len(branches_flow)-1):
+        print ('git checkout %s ' % (branches_flow[branch_index+1]))
         process = subprocess.Popen('git checkout %s ' % (branches_flow[branch_index+1]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        print('git merge %s ' % (current_branch))
         print(process.stdout.readlines())
-    # for index  in range(branch_index+1,len(branches_flow)):
-    #     process = subprocess.Popen('git checkout %s ' % (branches_flow[index]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    #     print(process.stdout.readlines())
-    #     process = subprocess.Popen('git merge %s ' % (current_branch), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    #     print(process.stdout.readlines())
-    #     if(not process.returncode):
-    #         process = subprocess.Popen('git push', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        
-
+        process1 = subprocess.Popen('git merge %s -m \"auto merge\"' % (current_branch), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        print(process1.stdout.readlines())
+        process1.communicate()[0]
+        return (process1.returncode)
 
 current_branch = ((os.popen("git rev-parse --abbrev-ref HEAD")).read()).rstrip()
-automatic_merge(current_branch, branches_flow)
+status = automatic_merge(current_branch, branches_flow)
